165f9ec5e8886337d9b0172a5ee24f621e7e5429
LauraLaureus <l4depende@gmail.com>
2015-05-05 19:13:19 +0100

    hgfiobfnio

--- JavaApplication2/src/primos/GeneradorCompuestos.java
+++ JavaApplication2/src/primos/GeneradorCompuestos.java
@@ -4,11 +4,9 @@ import java.util.ArrayList;
 
 public class GeneradorCompuestos extends ArrayList<Integer> {
 
-    public GeneradorCompuestos() {
-        this.add((int) (Math.random() * Integer.MAX_VALUE));
-    }
+   
     
     public Integer dameCompuesto(){
-        return 2*(int) (Math.random() * Integer.MAX_VALUE);
+        return 2*((int) (Math.random()*2000));
     }
 }
